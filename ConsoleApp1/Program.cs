3f57426c9a60fedd2531617eca0e2052e82def99
targino <contatobrunatargino@gmail.com>
2023-05-24 15:46:33 -0300

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+
+    {
+        enum Opcao { Criar = 1, Deletar, Editar, Listar, Atualizar }
+
+        static void Main(string[] args)
+        {
+            Opcoes();
+        }
+
+        static void Opcoes()
+        {
+            Console.WriteLine("Selecione uma das opções abaixo:");
+            Console.WriteLine("1 - Criar\n2 - Deletar\n3 - Editar\n4 - Listar\n5 - Atualizar");
+
+            int index = int.Parse(Console.ReadLine());
+            Opcao opcaoSelecionada = (Opcao)index;
+
+            switch (opcaoSelecionada)
+            {
+                case Opcao.Criar:
+                    Console.WriteLine("Você escolheu a opção de Criar!");
+                    break;
+
+                case Opcao.Deletar:
+                    Console.WriteLine("Você escolheu a opção de Deletar!");
+                    break;
+
+                case Opcao.Editar:
+                    Console.WriteLine("Você escolheu a opção de Editar!");
+                    break;
+
+                case Opcao.Listar:
+                    Console.WriteLine("Você escolheu a opção de Listar!");
+                    break;
+
+                case Opcao.Atualizar:
+                    Console.WriteLine("Você escolheu a opção de Atualizar!");
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+
+            Console.WriteLine("Sair do programa? S/N");
+            string respostaUser = Console.ReadLine();
+
+            switch (respostaUser)
+            {
+                case "S":
+                    break;
+
+                case "N":
+                    Opcoes();
+                    Console.ReadLine();
+                    break;
+            }
+        }
+    }
+}
